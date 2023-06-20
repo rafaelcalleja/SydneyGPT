359e84256caa90be90012b7616c67a41f45b54da
Rafael Calleja <rafaelcalleja@gmail.com>
2023-06-20 13:06:26 +0200

    upgrade version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ if not PATH.exists():
 
 setup(
     name="SydneyGPT",
-    version="0.11.1",
+    version="0.11.2",
     license="GNU General Public License v2.0",
     author="Rafael Calleja",
     author_email="rafaelcalleja@gmail.com",
